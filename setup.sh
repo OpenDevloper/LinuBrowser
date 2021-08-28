02022e5168a379cb97baff532d33bab9aad846d9
OpenDevloper <89633504+OpenDevloper@users.noreply.github.com>
2021-08-28 22:23:41 +0530

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,6 @@
+echo welcome to LinuBrowser Setup this program will install the dependencies required to run LinuBrowser
+sudo apt install python3-pip
+sudo pip install PyQt5
+sudo pip install PyQtWebEngine
+echo the script has installed the dependencies now you can close the terminal and run LinuBrowser!
+
